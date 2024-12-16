42ebf49fb7a68362d7d6ac8bf553acdd7be7c0f1
gastruc <guillaume.astruc.06@gmail.com>
2024-12-16 14:20:02 +0100

    update hubconf

--- hubconf.py
+++ hubconf.py
@@ -22,7 +22,7 @@ sys.path.append(str(REPO_ROOT / "src"))
 if not (REPO_ROOT / "models").exists():
     os.symlink(REPO_ROOT / "src/models", REPO_ROOT / "models")
 
-dependencies = ['torch', 'torchvision', 'omegaconf', 'timm']
+dependencies = ['torch', 'torchvision', 'omegaconf', 'timm', 'easydict', 'omegaconf', 'hydra-core']
 
 # Load base configurations
 CONFIG_PATH = REPO_ROOT / "configs"
